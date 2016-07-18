28509552fb08335e202f94e710fd30652ebe62bd
Dimple <dimpleperla@gmail.com>
2016-07-18 23:45:20 +0200

    Styling details

--- src/main/java/com/example/repository/PersonRepository.java
+++ src/main/java/com/example/repository/PersonRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-/**
- * Created by professor on 20/06/2016.
- */
 public interface PersonRepository extends JpaRepository<Person,Long> {
 
     List<Person> findByName(String name);
